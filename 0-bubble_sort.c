06647ee85649cf573b66de052ea888feb8f19878
edisson11osorio <osorioedisson08@gmail.com>
2022-02-15 17:41:15 -0500

    file-o-update

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,5 +1,23 @@
 #include "sort.h"
 
+void bubble_sort(int *array, size_t size)
+{
+	size_t j, i;
+	int n;
 
-
-void bubble_sort(int *array, size_t size);
+	if (size < 2)
+		return;
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				n = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = n;
+				print_array(array, size);
+			}
+		}
+	}
+}
